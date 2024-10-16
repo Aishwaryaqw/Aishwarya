f9585610762592de0730aa5eb08ae68e3aad00d3
Aishwaryaqw <166393397+Aishwaryaqw@users.noreply.github.com>
2024-10-16 06:48:50 -0700

    Update long_responses.py

--- long_responses.py
+++ long_responses.py
@@ -1,13 +1,4 @@
-import random
-
-R_EATING = "I don't like eating anything because I'm a bot obviously!"
-R_ADVICE = "If I were you, I would go to the internet and type exactly what you wrote there!"
-
-
-def unknown():
-    response = ["Could you please re-phrase that? ",
-                "...",
-                "Sounds about right.",
-                "What does that mean?"][
-        random.randrange(4)]
-    return response
+import openai
+openai.api_key = "sk-59NUZtqWHiBDn-5RbtG7XCaQSEPJhCEiidOO8w_qjfT3BlbkFJRpo1MKpR_XdIapiaNF0pvdDA4rRnO0Jz702qzV5XIA"
+completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Give me 3 ideas for apps I could build with openai apis "}])
+print(completion.choices[0].message.content)
